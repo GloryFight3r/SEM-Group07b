5c0b174f43daa448e3ce49c12925d62ec00263c6
Raul Cotar <raulcotar00@gmail.com>
2022-12-06 12:17:44 +0100

    Comment out broken test

---
+++ example-microservice/src/main/java/pizzeria/example/Application.java
@@ -0,0 +1,14 @@
+package pizzeria.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Example microservice application.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
