ebde35c1bc13d5a6453878eb459911237c625797
Laurens Michielsen <L.L.Michielsen@student.tudelft.nl>
2022-12-20 20:52:03 +0100

    Added tests to Authentication part

--- food-microservice/src/main/java/pizzeria/food/authentication/AuthManager.java
+++ food-microservice/src/main/java/pizzeria/food/authentication/AuthManager.java
@@ -1,19 +1,11 @@
 package pizzeria.food.authentication;
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class AuthManager {
 
-        /**
-        * Get the current user.
-        *
-        * @return The current user.
-        */
-        public String getCurrentUser() {
-            return SecurityContextHolder.getContext().getAuthentication().getName();
-        }
 
 
 }
---
+++ food-microservice/src/test/java/pizzeria/food/controllers/AllergenControllerTest.java
@@ -0,0 +1,13 @@
+package pizzeria.food.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AllergenControllerTest {
+
+    @Test
+    void filterMenu() {
+
+    }
+}
